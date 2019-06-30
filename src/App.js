880baa6a07e30fd7c0a61bffd97ba8687a960dfd
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2019-06-30 14:12:17 -0700

    Final state

--- src/App.js
+++ src/App.js
@@ -2,22 +2,36 @@ import * as React from 'react';
 import styles from './App.module.css';
 
 function Counter() {
-  return <div className={styles.wrapper}>0</div>;
+  const [count, updateCount] = React.useState(0);
+  React.useEffect(() => {
+    const timerID = setInterval(() => {
+      updateCount(count => count + 1);
+    }, 1000);
+    return () => {
+      clearInterval(timerID);
+    };
+  }, []);
+  return <div className={styles.wrapper}>{count}</div>;
 }
 
-function Button({onClick}) {
+const Button = React.memo(({onClick}) => {
+  console.log('rendering button');
   return (
     <button onClick={onClick} className={styles.button}>
       Toggle Counter
     </button>
   );
-}
+});
 
 function App() {
+  const [showCounter, setState] = React.useState(true);
+  const handleClick = React.useCallback(() => {
+    setState(showCounter => !showCounter);
+  }, []);
   return (
     <>
-      <Button />
-      <Counter />
+      <Button onClick={handleClick} />
+      {showCounter ? <Counter /> : null}
     </>
   );
 }
