7466bff2a2cd24b8a02d95fd2073e3a0f82f1967
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2019-07-01 12:17:07 -0700

    Comparing classes vs functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import * as React from 'react';
 import styles from './App.module.css';
 
+class CounterClass extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      this.setState(({count}) => ({count: count + 1}));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  render() {
+    return <div className={styles.wrapper}>{this.state.count}</div>;
+  }
+}
+
 function Counter() {
   const [count, updateCount] = React.useState(0);
   React.useEffect(() => {
@@ -14,14 +34,42 @@ function Counter() {
   return <div className={styles.wrapper}>{count}</div>;
 }
 
+class ButtonClass extends React.PureComponent {
+  render() {
+    return (
+      <button onClick={this.props.onClick} className={styles.button}>
+        Toggle Counter
+      </button>
+    );
+  }
+}
+
 const Button = React.memo(({onClick}) => {
-  console.log('rendering button');
   return (
     <button onClick={onClick} className={styles.button}>
       Toggle Counter
     </button>
   );
 });
+
+export class AppClass extends React.Component {
+  state = {
+    showCounter: true,
+  };
+
+  handleClick = () => {
+    this.setState(({showCounter}) => ({showCounter: !showCounter}));
+  };
+
+  render() {
+    return (
+      <>
+        <ButtonClass onClick={this.handleClick} />
+        {this.state.showCounter ? <CounterClass /> : null}
+      </>
+    );
+  }
+}
 
 function App() {
   const [showCounter, setState] = React.useState(true);
